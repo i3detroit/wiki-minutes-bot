bea95fd120362d449c8ddf8d4220747007c5ee6d
Mara Furland <mara@fur.land>
2026-01-06 13:26:46 -0500

    emails work

--- email_meeting_minutes.py
+++ email_meeting_minutes.py
@@ -9,79 +9,77 @@
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+from smtplib import SMTP_SSL
 import pywikibot
 import datetime
 import re
 import pickle
-from email.mime.text import MIMEText
+from email.message import EmailMessage
 import base64
 from pywikibot import pagegenerators
 
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 SENDTO = 'i3detroit@googlegroups.com'
-SECNAME = 'Secretary'
-MEMBER_EMAIL_TEMPLATE = '''The next i3Detroit member meeting will be {} at 7:30 pm. Meetings are hybrid meaning simultaneously online & in person. Please add announcements and discussion topics to the agenda below. Zone coordinators, please fill out the zone update for your zone.
-<br><br>
+SENDFROM = 'noreply@i3detroit.org'
+SIGNATURE = 'the it cabal'
+MEMBER_EMAIL_TEMPLATE = '''The next i3Detroit member meeting will be {} at 19:30.
+Meetings are hybrid meaning simultaneously online & in person.
+Please add announcements and discussion topics to the agenda below.
+Zone coordinators, please fill out the zone update for your zone.
+
 Agenda link: https://www.i3detroit.org/wiki/{}
-<br><br>
+
 See the following HOWTO for guidelines and tips for adding new agenda topics: https://www.i3detroit.org/wiki/HOWTO_Add_a_Topic_to_Meeting_Minutes_on_the_Wiki
-<br><br>
-You can join the meeting remotely on Google Meet at https://www.i3detroit.org/meeting. In virtual meetings, use the text chat for raising your hand. Please avoid conversation there so there are not multiple conversations happening at once. Attendance and votes happen in the #meetings slack channel: https://i3detroit.slack.com/archives/C0101CX23LN.
-<br><br>
-Thanks,<br>
+
+You can join the meeting remotely on Google Meet at https://www.i3detroit.org/meeting.
+In virtual meetings, use the text chat for raising your hand.
+Please avoid conversation there so there are not multiple conversations happening at once.
+Attendance and votes happen in the #meetings slack channel: https://i3detroit.slack.com/archives/C0101CX23LN.
+
+Thanks,
 {}'''
-BOARD_EMAIL_TEMPLATE = '''The next i3Detroit Board of Directors meeting will be {} at 7:30 pm. Meetings are held virtually on Google Meet. Please add discussion topics to the agenda below. Officers, please fill out the your officer reports.
-<br><br>
+BOARD_EMAIL_TEMPLATE = '''The next i3Detroit Board of Directors meeting will be {} at 19:30.
+Meetings are hybrid meaning simultaneously online & in person.
+Please add discussion topics to the agenda below.
+Officers, please fill out the your officer reports.
+
 Agenda link: https://www.i3detroit.org/wiki/{}
-<br><br>
+
 See the following HOWTO for guidelines and tips for adding new agenda topics: https://www.i3detroit.org/wiki/HOWTO_Add_a_Topic_to_Meeting_Minutes_on_the_Wiki
-<br><br>
-You can join the meeting remotely on Google Meet at https://www.i3detroit.org/meeting. In virtual meetings, use the text chat for raising your hand. Please avoid conversation there so there are not multiple conversations happening at once. Attendance and votes happen in the #meetings slack channel: https://i3detroit.slack.com/archives/C0101CX23LN.
-<br><br>
-Thanks,<br>
+
+You can join the meeting remotely on Google Meet at https://www.i3detroit.org/meeting.
+In virtual meetings, use the text chat for raising your hand.
+Please avoid conversation there so there are not multiple conversations happening at once.
+Attendance and votes happen in the #meetings slack channel: https://i3detroit.slack.com/archives/C0101CX23LN.
+
+Thanks,
 {}'''
 
 
-def write_message(page, meeting_type, meeting_date):
+def send_message(page, meeting_type, meeting_date):
     ''' Write appropriate meeting minutes email '''
     if meeting_type == 'member':
         subject = "{:%B %Y} First Tuesday Member Meeting - Call for Topics".format(meeting_date)
-        message_text = MEMBER_EMAIL_TEMPLATE.format('{:%A, %B %d, %Y}'.format(meeting_date), re.sub(" ", "_", page.title()), SECNAME)
+        message_text = MEMBER_EMAIL_TEMPLATE.format('{:%A, %B %d, %Y}'.format(meeting_date), re.sub(" ", "_", page.title()), SIGNATURE)
 
     elif meeting_type == 'board':
         subject = "{:%B %Y} Board of Directors Meeting - Call for Topics".format(meeting_date)
-        message_text = BOARD_EMAIL_TEMPLATE.format('{:%A, %B %d, %Y}'.format(meeting_date), re.sub(" ", "_", page.title()), SECNAME)
+        message_text = BOARD_EMAIL_TEMPLATE.format('{:%A, %B %d, %Y}'.format(meeting_date), re.sub(" ", "_", page.title()), SIGNATURE)
     else:
         print("Not a member meeting or board meeting. Nothing to send.")
         exit()
 
-    message = MIMEText(message_text, 'html')
+    message = EmailMessage()
+    message.set_content(message_text)
     message['to'] = SENDTO
-    message['from'] = 'i3 Secretary'
+    message['from'] = SENDFROM
     message['subject'] = subject
-    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
-
-
-def send_message(service, message):
-    '''Send an email message.
-
-    Args:
-        service: Authorized Gmail API service instance.
-        message: Message to be sent.
-
-    Returns:
-        Sent Message.
-    '''
     print('{now} Sending message...'.format(now=datetime.datetime.now()))
-    try:
-        with SMTP_SSL(host="smtp-relay.gmail.com") as s:
-            s.sendmail("noreply@i3detroit.org", "mtfurlan@i3detroit.org", message)
-            # TESTING: todo change
-        print('Meeting email sent.')
-        return message
-    except HttpError as error:
-        print('An error occurred sending meeting email: %s' % error)
+    with SMTP_SSL(host="smtp-relay.gmail.com") as s:
+        s.send_message(message)
+        s.quit()
+    print('Meeting email sent.')
+
 
 
 if __name__ == '__main__':
@@ -101,6 +99,6 @@ if __name__ == '__main__':
         minuteDate = datetime.datetime.strptime(date, '%Y%m%d')
         if(futureDate == minuteDate.date()):
             if 'Minutes:Meeting Minutes' in page.title():
-                send_message(write_message(page, 'member', minuteDate))
+                send_message(page, 'member', minuteDate)
             elif 'Minutes:Board Meeting Minutes' in page.title():
-                send_message(write_message(page, 'board', minuteDate))
+                send_message(page, 'board', minuteDate)
